7889ec5d8060a95c44fb4f916a53589aeecffb7c
Osman Mazariegos Local <osman.mazariegos@grupogente.com>
2021-09-21 06:44:15 -0600

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,38 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MatSliderModule } from '@angular/material/slider';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTabsModule } from '@angular/material/tabs';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatCardModule} from '@angular/material/card';
+import {MatSidenavModule} from '@angular/material/sidenav';
+
+import {MatTableModule} from '@angular/material/table';
+
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSliderModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTabsModule,
+    MatGridListModule,
+    MatCardModule,
+    MatSidenavModule,
+    MatTableModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
